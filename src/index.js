e053acf22ace56c85a3842ed8ab1fad560a0f6f7
Ibtesum Reza Aninda <aninda7152@gmail.com>
2022-09-03 20:30:38 +0600

    se

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM  from "react-dom";
 import App from './App'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
-
-
-
-
-// #react learning
+ReactDOM.render(
+    <React.StrictMode>
+        <App/> 
+    </React.StrictMode>,
+    document.getElementById('root'))
