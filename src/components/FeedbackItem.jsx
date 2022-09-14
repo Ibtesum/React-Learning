38d433451e6e1adfc40957c4bbe5ffc7d7a02cc1
Ibtesum Reza Aninda <aninda7152@gmail.com>
2022-09-15 03:18:29 +0600

    upto FeedbackStats

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,15 @@
+import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 
 import Card from "./shared/Card"
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
 
     return (
         <Card reverse={true}>
             <div className="num-display">{item.rating}</div>
+            <button onClick={(id) => handleDelete(item.id)} className="close">
+                <FaTimes color='purple' />
+            </button>
             <div className="text-display">
                 {item.text}
             </div>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
-function  FeedbackList({feedback}) {
+function  FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0){
         return <p>No feedback yet</p>
     }
@@ -8,7 +8,9 @@ function  FeedbackList({feedback}) {
     return (
     <div className="feedback-list">
         {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item}/>
+            <FeedbackItem key={item.id} item={item}
+            handleDelete={handleDelete}
+            />
         ))}
     </div>
   )
