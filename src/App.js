5ebc80d6556bc0afbb6fffc4e1097a8a35e7ca4b
Ibtesum Reza Aninda <aninda7152@gmail.com>
2022-09-20 10:20:07 +0600

    framer motion added 0406

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {v4 as uuidv4} from 'uuid'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackData from "./data/FeedbackData"
@@ -7,17 +8,25 @@ import FeedbackForm from "./components/FeedbackForm"
 
 function App(){
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) =>{
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
+
+
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackForm />
+                <FeedbackForm handleAdd = {addFeedback}/>
                 <FeedbackStats feedback= {feedback} />
                 <FeedbackList feedback={feedback}
                 handleDelete= {deleteFeedback}/>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 function  FeedbackList({feedback, handleDelete}) {
@@ -5,14 +6,33 @@ function  FeedbackList({feedback, handleDelete}) {
         return <p>No feedback yet</p>
     }
 
+    // framer-motion added
     return (
-    <div className="feedback-list">
-        {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item}
-            handleDelete={handleDelete}
-            />
-        ))}
-    </div>
+        <div className="feedback-list">
+            <AnimatePresence>
+            {feedback.map((item) => (
+                <motion.div 
+                    key={item.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                >
+                    <FeedbackItem key={item.id} item={item}
+                    handleDelete={handleDelete}
+                    />
+                </motion.div>
+            ))}
+            </AnimatePresence>
+        </div>
+    // without framer motion
+    // return (
+    // <div className="feedback-list">
+    //     {feedback.map((item) => (
+    //         <FeedbackItem key={item.id} item={item}
+    //         handleDelete={handleDelete}
+    //         />
+    //     ))}
+    // </div>
   )
 }
 
